36dcc5f93aea4d62b8c901b0a197ac9bd4519bbe
mespinozas <mario@polinom.io>
2020-07-18 20:30:20 -0400

    deault structre

---
+++ scripts/replace-tslint.sh
@@ -0,0 +1,7 @@
+npm i -D eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser
+
+npx tslint-to-eslint-config
+
+npm install @angular-eslint/eslint-plugin @angular-eslint/eslint-plugin-template @typescript-eslint/eslint-plugin-tslint eslint-plugin-import eslint-plugin-jsdoc eslint-plugin-prefer-arrow --save-dev
+
+npx eslint --init
---
+++ scripts/use-eslint-prettier.sh
@@ -0,0 +1,8 @@
+# Install ESLint
+npm install --save-dev eslint
+
+# Install additional plugins
+npm install --save-dev @typescript-eslint/eslint-plugin eslint-plugin-prettier
+
+# Install Prettier and Prettier-ESLint dependencies
+npm install --save-dev prettier prettier-eslint eslint-config-prettier
